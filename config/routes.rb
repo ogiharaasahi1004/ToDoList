5352dc2206311f05cdb2c00c99e50d606718168e
asahi.ogihara <t19a014e@mail.cc.niigata-u.ac.jp>
2021-02-15 07:53:46 +0000

    user signup

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   get 'toppages/index'
   root to: 'tasks#index'
 
+  get 'signup', to: 'users#new'
+  resources :users, only: [:index, :show, :create]
   resources :tasks
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
